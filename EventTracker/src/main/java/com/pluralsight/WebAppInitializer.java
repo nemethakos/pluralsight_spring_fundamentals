c6f74a43939c6e1d38a99a46e06ad6f37bfc8f08
Ákos Németh <nemeth.akos@gmail.com>
2018-07-21 01:15:35 +0200

    Spring MVC 4 Course Chapters 1-4 Initial Commit

---
+++ EventTracker/src/main/java/com/pluralsight/WebAppInitializer.java
@@ -0,0 +1,38 @@
+package com.pluralsight;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+		WebApplicationContext rootContext = getContext();
+		
+		container.addListener(new ContextLoaderListener(rootContext));
+		
+		ServletRegistration.Dynamic dispatcher = 
+				container.addServlet("DispatcherServlet",
+				new DispatcherServlet(rootContext));
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("*.html");
+	}
+
+	private AnnotationConfigWebApplicationContext getContext() {
+		AnnotationConfigWebApplicationContext rootContext = 
+				new AnnotationConfigWebApplicationContext();
+		// for Spring 4 this is used
+		// rootContext.setConfigLocation("com.pluralsight.WebConfig");
+		// for Spring 5 this is needed 
+		rootContext.register(WebConfig.class);
+		return rootContext;
+	}
+
+}
---
+++ EventTracker/src/main/webapp/index.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Index</title>
+</head>
+<body>
+	<h1>Hello!</h1>
+	<ul>
+		<li><a href="greeting.html">greeting.html</a>
+	</ul>
+</body>
+</html>
