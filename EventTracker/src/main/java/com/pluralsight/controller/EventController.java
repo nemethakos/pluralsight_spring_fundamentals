b2e714020f92baafe2fb44ef9d0b46530a55abf0
Ákos Németh <nemeth.akos@gmail.com>
2018-07-22 04:58:54 +0200

    Spring MVC 4/Chapter 5

---
+++ EventTracker/src/main/java/com/pluralsight/controller/EventController.java
@@ -0,0 +1,22 @@
+package com.pluralsight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.pluralsight.model.Event;
+
+@Controller
+public class EventController {
+
+	@RequestMapping(value = "/event", method = RequestMethod.GET)
+	public String displayEventPage(Model model) {
+		var event = new Event();
+		event.setName("Java User Group");
+		model.addAttribute("event",event);
+		
+		return "event";
+	}
+	
+}
